1f7268dc27e0fc3066ad93f73d1372cccb556844
Kafilat <kafilatadewumi61@gmail.com>
2023-02-05 22:10:01 +0100

    fixes

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,23 +1,35 @@
-import React, {Fragment, useState} from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import "./Navbar.css"
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const navItem = "about,services,projects,contacts".split(",");
 
 const Navbar = () => {
-  const [showNav, setShowNav] = useState(false)
-  const [color, setColor]= useState(false)
-  const changeColor = () => {
-    if (window.scrollY >= 50) {
-      setColor(true)
-    } else {
-      setColor(false)
-    }
+  const [showNav, setShowNav] = useState(false);
+  const [color] = useState(false);
+  const [bg, setbgcolor] = useState("transparent")
+
+  const changeColor=()=>{
+      console.log("I got here");
+
+    let windowHeight= window.scrollY;
+    console.log(windowHeight)
+    windowHeight > 500 ? setbgcolor("red") : setbgcolor("black");
   }
-  window.addEventListener('scroll', changeColor)
+
+
+  useEffect(() => {
+    const onScroll = (event) => console.info("scrolling", event);
+      
+    window.addEventListener('scroll', onScroll);
+    
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    }
+  }, []);
 
   return (
-    <nav className={`nav-items ${color} ? nav-header : nav-items`}>
+    <nav  className={!color ? "nav-items nav-header" : "nav-items"}>
       <Link to="/">
         <div className="left">
           <div className="circle"></div>
@@ -36,19 +48,23 @@ const Navbar = () => {
         <button className="project">Need a project?</button>
       </a>
 
-      <p className="block md:hidden" onClick={()=>setShowNav(!showNav)}>
+      <p className="block md:hidden" onClick={() => setShowNav(!showNav)}>
         <img src="/menu.svg" alt="" />
       </p>
-        <div className={`absolute capitalize ${showNav?"right-0":"-right-24"}  h-60 w-24 top-20 transition-all duration-150`}>
-          {navItem.map((Item, ItemIndex) => (
-            <Link to={`/${Item}`} key={ItemIndex}>
-              {" "}
-              {Item}
-            </Link>
-          ))}
-        </div>
+      <div
+        className={`absolute capitalize ${
+          showNav ? "right-0" : "-right-24"
+        }  h-60 w-24 top-20 transition-all duration-150`}
+      >
+        {navItem.map((Item, ItemIndex) => (
+          <Link to={`/${Item}`} key={ItemIndex}>
+            {" "}
+            {Item}
+          </Link>
+        ))}
+      </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
--- src/Components/PageLayout.jsx
+++ src/Components/PageLayout.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import { useEffect } from 'react';
 
 const PageLayout = ({ children}) => {
+  console.log('right file')
+  useEffect(() => {
+    window.addEventListener('scroll', e => {
+      console.log({ e });
+    })
+  }, [])
   return (
-    <div className="flex flex-col w-screen h-screen overflow-hidden ">
+    <div className="flex flex-col w-screen h-screen overflow-y-auto overflow-x-hidden">
       <Navbar/>
       <main className="flex-grow overflow-auto">
         {children}
