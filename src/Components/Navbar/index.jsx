8c814266e4780379c2c2ef0ae502db6f1d71c2bf
ADEWUMI, KAFILAT ENIOLA <102726940+DevKaffy@users.noreply.github.com>
2023-02-16 18:48:41 +0100

    Update index.jsx

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -7,61 +7,66 @@ const navItem = "about,services,projects,contacts".split(",");
 const Navbar = () => {
   const [showNav, setShowNav] = useState(false);
   const [color] = useState(false);
-  const [bg, setbgcolor] = useState("transparent")
+  const [bg, setbgcolor] = useState("transparent");
 
-  const changeColor=()=>{
-      console.log("I got here");
+  const changeColor = () => {
+    console.log("I got here");
 
-    let windowHeight= window.scrollY;
-    console.log(windowHeight)
+    let windowHeight = window.scrollY;
+    console.log(windowHeight);
     windowHeight > 500 ? setbgcolor("red") : setbgcolor("black");
-  }
-
+  };
 
   useEffect(() => {
     const onScroll = (event) => console.info("scrolling", event);
-      
-    window.addEventListener('scroll', onScroll);
-    
+
+    window.addEventListener("scroll", onScroll);
+
     return () => {
-      window.removeEventListener('scroll', onScroll);
-    }
+      window.removeEventListener("scroll", onScroll);
+    };
   }, []);
 
   return (
-    <nav  className={!color ? "nav-items nav-header" : "nav-items"}>
-      <Link to="/">
-        <div className="left">
-          <div className="circle"></div>
-          <p className="build">Buildit</p>
+    <nav className={color ? "nav-header" : ""}>
+      <div className="nav-items center">
+        <Link to="/">
+          <div className="left">
+            <div className="circle"></div>
+            <p className="build">Buildit</p>
+          </div>
+        </Link>
+        <div className="items">
+          {navItem.map((Item, ItemIndex) => (
+            <Link to={`/${Item}`} key={ItemIndex}>
+              {" "}
+              {Item}
+            </Link>
+          ))}
         </div>
-      </Link>
-      <div className="items">
-        {navItem.map((Item, ItemIndex) => (
-          <Link to={`/${Item}`} key={ItemIndex}>
-            {" "}
-            {Item}
-          </Link>
-        ))}
-      </div>
-      <a href="#form">
-        <button className="project">Need a project?</button>
-      </a>
+        <a href="#form">
+          <button className="project">Need a project?</button>
+        </a>
 
-      <p className="block md:hidden" onClick={() => setShowNav(!showNav)}>
-        <img src="/menu.svg" alt="" />
-      </p>
-      <div
-        className={`absolute capitalize ${
-          showNav ? "right-0" : "-right-24"
-        }  h-60 w-24 top-20 transition-all duration-150`}
-      >
-        {navItem.map((Item, ItemIndex) => (
-          <Link to={`/${Item}`} key={ItemIndex}>
-            {" "}
-            {Item}
-          </Link>
-        ))}
+        {/*Mobile tab*/}
+        <>
+          <div className="block md:hidden" onClick={() => setShowNav(!showNav)}>
+            <img src="/menu.svg" alt="" />
+          </div>
+
+          <div
+            className={`absolute capitalize ${
+              showNav ? "right-0" : "-right-[15rem]"
+            }  h-60 w-24 top-20 transition-all duration-150`}
+          >
+            {navItem.map((Item, ItemIndex) => (
+              <Link to={`/${Item}`} key={ItemIndex}>
+                {" "}
+                {Item}
+              </Link>
+            ))}
+          </div>
+        </>
       </div>
     </nav>
   );
